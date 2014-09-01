3bf7381fa379996d58b54c9608d977d5a6be9609
Asjer Querido <asjer@mac.com>
2014-09-01 18:45:59 +0200

    commented out the default spec examples

--- spec/helpers/todo_lists_helper_spec.rb
+++ spec/helpers/todo_lists_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe TodoListsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#describe TodoListsHelper do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
